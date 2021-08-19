35283fea636b9bd99bae91d60bd8ddd04b47dc73
Wander Aquino <wander.aquino@gmail.com>
2021-08-19 20:26:47 -0300

    Adding search input field

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
-import {Flex, Text} from "@chakra-ui/react";
+import {Flex, Text, Input, Icon} from "@chakra-ui/react";
+import {RiSearchLine} from "react-icons/ri";
 
 export function Header () {
     return (
@@ -12,7 +13,39 @@ export function Header () {
             px="6"
             align="center"
         >
-            <Text>DashGo</Text>
+            <Text
+                fontSize="3xl"
+                fontWeight="bold"
+                letterSpacing="tight"
+                w="64"
+            > DashGo
+                <Text as="span" color="pink.500" ml="1">.</Text>
+            </Text>
+
+            <Flex 
+                as="label"
+                flex="1"
+                py="4"
+                px="8"
+                ml="6"
+                maxWidth={400}
+                alignSelf="center"
+                color="gray.200"
+                position="relative"
+                bg="gray.800"
+                borderRadius="full"
+            >
+                <Input
+                    color="gray.50"
+                    variant="unstyled"
+                    px="4"
+                    mr="4"
+                    placeholder="Buscar na plataforma"
+                    _placeholder={{color: "gray.400"}}
+                >
+                </Input>
+                <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
         </Flex>
     )
 }
