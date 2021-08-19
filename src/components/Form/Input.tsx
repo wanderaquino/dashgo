82fa60daea97c2fd0164e16061b7bb29f1eae80a
Wander Aquino <wander.aquino@gmail.com>
2021-08-19 19:52:16 -0300

    Tracking input component

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,25 @@
+import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps } from "@chakra-ui/react"
+
+interface InputProps extends ChakraInputProps {
+    name: string;
+    label?: string
+}
+
+export function Input(props: InputProps) {
+    return (
+        <FormControl>
+            {!!props.label && <FormLabel htmlFor={props.name}>{props.label}</FormLabel>}
+        <ChakraInput 
+          name={props.name}
+          id={props.name} 
+          type="email"
+          focusBorderColor="pink.500" 
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{bgColor:"gray.900"}}
+          size="lg"
+          {...props}
+        />
+      </FormControl>
+    )
+}
