4d25f97c56c408cce92bdde317bd544b44853e8f
Wander Aquino <wander.aquino@gmail.com>
2021-08-21 20:02:15 -0300

    Adding statical user data on users page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import {Box, Flex, Heading, Button, Icon} from "@chakra-ui/react";
+import {Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Td, Th, Checkbox, Tbody, Text} from "@chakra-ui/react";
 import {Header} from "../../components/Header/Header"
 import { Sidebar } from "../../components/Sidebar/Sidebar";
 import {RiAddLine } from "react-icons/ri";
@@ -13,8 +13,42 @@ export default function UserList() {
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">Usuários</Heading>
-                        <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} />}>Criar Novo</Button>
+                        <Button 
+                            as="a" 
+                            size="sm" 
+                            fontSize="sm" 
+                            colorScheme="pink" 
+                            leftIcon={<Icon as={RiAddLine} fontSize={20}/>}>
+                                Criar Novo
+                            </Button>
                     </Flex>
+                    <Table colorSchema="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink"></Checkbox>
+                                </Th>
+                                <Th>Usuário</Th>
+                                <Th>Data de Cadastro</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink"></Checkbox>
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Wander Aquino</Text>
+                                        <Text fontSize="sm" color="gray.300">wander.aquino@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>
+                                    03/08/2021
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
                 </Box>
             </Flex>
         </Box>
