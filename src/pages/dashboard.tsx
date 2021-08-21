4eedee3de591757030c91927488134f8444bb16c
Wander Aquino <wander.aquino@gmail.com>
2021-08-21 16:18:03 -0300

    Adding box for dashboards

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,5 @@
 import {Header} from "../components/Header/Header";
-import {Flex} from "@chakra-ui/react";
+import {Flex, SimpleGrid, Box, Text} from "@chakra-ui/react";
 import { Sidebar } from "../components/Sidebar/Sidebar";
 
 export default function Dashboard () {
@@ -8,6 +8,15 @@ export default function Dashboard () {
             <Header></Header>
             <Flex w="100%" my="6" maxWidth="1480" mx="auto" px="6">
                 <Sidebar />
+
+                <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+                    <Box p="8" bg="gray.800" borderRadius={8}>
+                        <Text fontSize="lg" mb="4">Inscritos da Semana</Text>
+                    </Box>
+                    <Box p="8" bg="gray.800" borderRadius={8}>
+                        <Text fontSize="lg" mb="4">Taxa de Abertura</Text>
+                    </Box>
+                </SimpleGrid>
             </Flex>
         </Flex>
     )
