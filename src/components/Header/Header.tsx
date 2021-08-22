e8c04c947bd7ff3c1c6a7b14856030d00a800f15
Wander Aquino <wander.aquino@gmail.com>
2021-08-22 15:01:55 -0300

    Rendering components on header container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
-import {Flex, Text, Input, Icon, Stack, Box, Avatar} from "@chakra-ui/react";
-import {RiNotificationLine, RiSearchLine, RiUserAddLine} from "react-icons/ri";
+import {Flex} from "@chakra-ui/react";
+import Logo from "./Logo";
 import { Profile } from "./Profile";
-
+import Search from "./Search";
+import { Notifications } from "./NotificationsNav";
 export function Header () {
     return (
         <Flex
@@ -12,59 +13,13 @@ export function Header () {
             mx="auto"
             mt="4"
             px="6"
-            align="center"
-        >
-            <Text
-                fontSize="3xl"
-                fontWeight="bold"
-                letterSpacing="tight"
-                w="64"
-            > DashGo
-                <Text as="span" color="pink.500" ml="1">.</Text>
-            </Text>
+            align="center">
 
-            <Flex 
-                as="label"
-                flex="1"
-                py="4"
-                px="8"
-                ml="6"
-                maxWidth={400}
-                alignSelf="center"
-                color="gray.200"
-                position="relative"
-                bg="gray.800"
-                borderRadius="full"
-            >
-                <Input
-                    color="gray.50"
-                    variant="unstyled"
-                    px="4"
-                    mr="4"
-                    placeholder="Buscar na plataforma"
-                    _placeholder={{color: "gray.400"}}
-                >
-                </Input>
-                <Icon as={RiSearchLine} fontSize="20" />
-            </Flex>
+            <Logo />
+            <Search />
 
-            <Flex 
-                align="center"
-                ml="auto"
-            >
-                <Stack 
-                    spacing="8" 
-                    direction="row"
-                    mx="8"
-                    pr="8"
-                    py="1"
-                    color="gray.300"
-                    borderRightWidth={1}
-                    borderColor="gray.700"
-                >
-                    <Icon as={RiNotificationLine} fontSize="20"></Icon>
-                    <Icon as={RiUserAddLine} fontSize="20"></Icon>
-                </Stack>
+            <Flex align="center" ml="auto">
+                <Notifications />
                 <Profile />
             </Flex>
         </Flex>
