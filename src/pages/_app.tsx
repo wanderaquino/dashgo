34de57829d00ed7afa8e3fb345b2af25286494f4
Wander Aquino <wander.aquino@gmail.com>
2021-08-22 19:28:35 -0300

    Adding SidebarProvider in app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import {AppProps} from "next/app";
 import {ChakraProvider} from "@chakra-ui/react";
 import {theme} from "../styles/theme";
+import { SiderbarProvider } from "../contexts/SidebarDrawerContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <SiderbarProvider>
+          <Component {...pageProps} />
+        </SiderbarProvider>
+      </ChakraProvider>
   )
-  
 }
 
 export default MyApp
