85fecea139710276e3c8182dd0ec98c4aaafffb2
Wander Aquino <wander.aquino@gmail.com>
2021-08-22 15:01:26 -0300

    Implementing Notifications component

---
+++ src/components/Header/NotificationsNav.tsx
@@ -0,0 +1,20 @@
+import { Stack, Icon,  } from "@chakra-ui/react"
+import { RiNotificationLine, RiUserAddLine  } from "react-icons/ri"
+
+export function Notifications () {
+    return (
+        <Stack 
+        spacing="8" 
+        direction="row"
+        mx="8"
+        pr="8"
+        py="1"
+        color="gray.300"
+        borderRightWidth={1}
+        borderColor="gray.700">
+            
+        <Icon as={RiNotificationLine} fontSize="20"></Icon>
+        <Icon as={RiUserAddLine} fontSize="20"></Icon>
+    </Stack>
+    )
+}
