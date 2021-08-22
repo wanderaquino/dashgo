762d4b1ec3d479da0e57385b43e9458d291cae03
Wander Aquino <wander.aquino@gmail.com>
2021-08-22 19:51:24 -0300

    Adding close drawer sidebar behavior

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,18 +1,21 @@
 import { Box, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, useBreakpointValue} from "@chakra-ui/react";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 import { SidebarNav } from "./SideBarNav";
 
 export function Sidebar() {
-    const isDrawerSidebar = useBreakpointValue({
+    const {isOpen, onClose} = useSidebarDrawer();
+
+    const isWideScreen = useBreakpointValue({
         base: true,
         lg: false
     });
 
-    if (isDrawerSidebar) {
+    if (isWideScreen) {
         return (
-        <Drawer isOpen={true} placement="left" onClose={() => {}}>
+        <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
             <DrawerOverlay>
                 <DrawerContent bg="gray.800" padding="4">
-                    <DrawerCloseButton mt="6" />
+                    <DrawerCloseButton mt="6"/>
                     <DrawerHeader>Navegação</DrawerHeader>
                     <DrawerBody>
                         <SidebarNav />
