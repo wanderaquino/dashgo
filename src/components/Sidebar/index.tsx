1160aed24035dad36c89ef41da1f7522d3ce8ba3
Wander Aquino <wander.aquino@gmail.com>
2021-08-22 19:02:36 -0300

    Extracting links to sidebarnav component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,24 +1,10 @@
-import { Box, Stack, Text, Link, Icon} from "@chakra-ui/react";
-import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
-import { NavLink } from "./NavLink";
-import { NavSection } from "./NavSection";
+import { Box} from "@chakra-ui/react";
+import { SidebarNav } from "./SideBarNav";
 
 export function Sidebar() {
     return (
         <Box as="aside" w="64" mr="8">
-            <Stack spacing="12" align="flex-start">
-                
-                <NavSection title="GERAL">
-                    <NavLink icon={RiDashboardLine} text="Dashboard" />
-                    <NavLink icon={RiContactsLine} text="Usuários" />
-                </NavSection>
-
-                <NavSection title="AUTOMAÇÃO">
-                        <NavLink icon={RiInputMethodLine} text="Formulários" />
-                        <NavLink icon={RiGitMergeLine} text="Automação" /> 
-                </NavSection>
-
-            </Stack>
+            <SidebarNav />
         </Box>
     )
 }
