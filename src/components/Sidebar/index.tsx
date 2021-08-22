371d9053db607062ff348bf17ee347b30dd6f81f
Wander Aquino <wander.aquino@gmail.com>
2021-08-22 19:29:24 -0300

    Adding sidebar responsive drawer menu

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,28 @@
-import { Box} from "@chakra-ui/react";
+import { Box, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, useBreakpointValue} from "@chakra-ui/react";
 import { SidebarNav } from "./SideBarNav";
 
 export function Sidebar() {
+    const isDrawerSidebar = useBreakpointValue({
+        base: true,
+        lg: false
+    });
+
+    if (isDrawerSidebar) {
+        return (
+        <Drawer isOpen={true} placement="left" onClose={() => {}}>
+            <DrawerOverlay>
+                <DrawerContent bg="gray.800" padding="4">
+                    <DrawerCloseButton mt="6" />
+                    <DrawerHeader>Navegação</DrawerHeader>
+                    <DrawerBody>
+                        <SidebarNav />
+                    </DrawerBody>
+                </DrawerContent>
+            </DrawerOverlay>
+        </Drawer>
+        )
+    }
+
     return (
         <Box as="aside" w="64" mr="8">
             <SidebarNav />
