c5081005f1bde2ddafbc71d6fa06611b49e78ecb
Wander Aquino <wander.aquino@gmail.com>
2021-08-23 23:03:34 -0300

    Adding responsive fontSize

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -3,10 +3,11 @@ import { Text } from "@chakra-ui/react";
 export default function Logo() {
     return (
         <Text
-        fontSize="3xl"
+        fontSize={["2xl","3xl"]}
         fontWeight="bold"
         letterSpacing="tight"
         w="64"
+        verticalAlign="top"
     > DashGo
         <Text as="span" color="pink.500" ml="1">.</Text>
     </Text>
