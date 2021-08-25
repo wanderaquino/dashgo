3ef5c6426bd6ef17468e83e93617bec4653b0801
Wander Aquino <wander.aquino@gmail.com>
2021-08-24 22:09:24 -0300

    Adding link component around the Create User button

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,6 +3,7 @@ import {Header} from "../../components/Header"
 import { Sidebar } from "../../components/Sidebar";
 import {RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination";
+import Link from "next/link";
 
 export default function UserList() {
     
@@ -19,14 +20,17 @@ export default function UserList() {
                 <Box flex="1" borderRadius={8} bg="gray.800" p="4">
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="bold">Usuários</Heading>
-                        <Button 
-                            as="a" 
-                            size="sm" 
-                            fontSize="sm" 
-                            colorScheme="pink">
-                            <Icon as={RiAddLine} fontSize={20}/>
-                            {isWideScreen && "Criar Usuário"}
+                        <Link href="/users/create">
+                            <Button 
+                                as="a" 
+                                size="sm" 
+                                fontSize="sm" 
+                                colorScheme="pink">
+                                <Icon as={RiAddLine} fontSize={20}/>
+                                {isWideScreen && "Criar Usuário"}
                             </Button>
+                        </Link>
+
                     </Flex>
                     {!isWideScreen ? (
                      <Table colorScheme="whiteAlpha">
