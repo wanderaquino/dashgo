18fd91cdadf5ea70ebc63a0f112b390fd43474e4
Wander Aquino <wander.aquino@gmail.com>
2021-08-24 21:39:04 -0300

    Adding next Link component

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,15 +1,20 @@
-import { Link, Icon, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import { Link as ChakraLink, Icon, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
 import { ElementType } from "react";
+import Link from "next/link";
+
 
 interface NavLinkProps extends ChakraLinkProps {
     text: string,
-    icon: ElementType
+    icon: ElementType,
+    href: string
 }
-export function NavLink({text, icon, ...rest} : NavLinkProps) {
+export function NavLink({text, icon, href, ...rest} : NavLinkProps) {
     return (
-    <Link display="flex" align="center" {...rest}>
-        <Icon as={icon} fontSize="20" />
-        <Text ml="4" fontWeight="medium" color="gray.400">{text}</Text>    
-    </Link>
+        <Link href={href}>
+            <ChakraLink display="flex" align="center" {...rest}>
+                <Icon as={icon} fontSize="20" />
+                <Text ml="4" fontWeight="medium" color="gray.400">{text}</Text>    
+            </ChakraLink>
+        </Link>
     )
 }
