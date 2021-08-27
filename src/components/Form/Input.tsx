ea176ba9a6111df6361ea478d71d0be06d898230
Wander Aquino <wander.aquino@gmail.com>
2021-08-26 21:33:22 -0300

    Displaying input format error message

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,25 +1,35 @@
-import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps } from "@chakra-ui/react"
+import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps, FormErrorMessage } from "@chakra-ui/react"
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps {
     name: string;
     label?: string
+    error?: FieldError
 }
 
-export function Input(props: InputProps) {
+export function Input({name, label, error, ...rest}: InputProps) {
+
     return (
-        <FormControl>
-            {!!props.label && <FormLabel htmlFor={props.name}>{props.label}</FormLabel>}
-        <ChakraInput 
-          name={props.name}
-          id={props.name} 
-          type={props.name}
-          focusBorderColor="pink.500" 
-          bgColor="gray.900"
-          variant="filled"
-          _hover={{bgColor:"gray.900"}}
-          size="lg"
-          {...props}
-        />
+        <FormControl isInvalid={!!error}>
+            {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+          <ChakraInput 
+            name={name}
+            id={name} 
+            type={name}
+            focusBorderColor="pink.500" 
+            bgColor="gray.900"
+            variant="filled"
+            _hover={{bgColor:"gray.900"}}
+            size="lg"
+            {...rest}
+          />
+
+          {!!error && (
+          <FormErrorMessage>
+            {error.message}
+          </FormErrorMessage>
+          )
+          }
       </FormControl>
     )
 }
