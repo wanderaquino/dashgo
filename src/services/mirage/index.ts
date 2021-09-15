25ed3790ff05abd0b757556d309f3b27c0d2936f
Wander Aquino <wander.aquino@gmail.com>
2021-09-15 00:29:30 -0300

    Track miragejs /user route

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,28 @@
+import { createServer, Model} from "miragejs";
+
+type User = {
+    name: string,
+    email: string,
+    created_at: string
+}
+
+export function makeServer () {
+    const server = createServer({
+        models: {
+            user: Model.extend<Partial<User>>({})
+        },
+        routes() {
+            //Time to fetch data in ms...
+            this.timing = 850;
+            this.namespace = "api";
+            this.get("/users");
+            this.post("/users")
+            this.namespace="";
+
+
+            this.passthrough();
+        }
+    })
+
+    return server;
+}
