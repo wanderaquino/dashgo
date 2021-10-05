34e5ac08e901d33f3b0de547b333e29dc23d3dd9
Wander Aquino <wander.aquino@gmail.com>
2021-10-04 21:26:00 -0300

    Use RestSerialize for miragejs

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response} from "miragejs";
+import { createServer, Factory, Model, Response, RestSerializer} from "miragejs";
 
 type User = {
     name: string,
@@ -8,6 +8,9 @@ type User = {
 
 export function makeServer () {
     const server = createServer({
+        serializers: {
+            application: RestSerializer
+        },
         models: {
             user: Model.extend<Partial<User>>({})
         },
